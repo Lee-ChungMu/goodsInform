e04e806c984871bf911413676e246fa94e4b5020
이충무 <lcm2822@daum.net>
2022-06-10 14:43:37 +0900

    프로젝트 생성

---
+++ goodsInform/src/main/java/leechungmu/goodsInform/GoodsInformApplication.java
@@ -0,0 +1,13 @@
+package leechungmu.goodsInform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsInformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsInformApplication.class, args);
+	}
+
+}
---
+++ goodsInform/src/test/java/leechungmu/goodsInform/GoodsInformApplicationTests.java
@@ -0,0 +1,13 @@
+package leechungmu.goodsInform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsInformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
