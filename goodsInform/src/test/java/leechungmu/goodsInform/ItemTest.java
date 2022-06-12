f25ea4eafcf9d6d139f8f2c47a3f9fb191ed1d62
이충무 <lcm2822@daum.net>
2022-06-12 17:16:37 +0900

    추가 : ItemTest에 사용자가 구매할 수 있는 상품 정보를 반환 테스트 추가, PromotionTest에 상품에 존재하는 프로모션 정보를 반환 테스트 추가

--- goodsInform/src/test/java/leechungmu/goodsInform/ItemTest.java
+++ goodsInform/src/test/java/leechungmu/goodsInform/ItemTest.java
@@ -3,12 +3,16 @@ package leechungmu.goodsInform;
 
 import leechungmu.goodsInform.entity.Dto.ItemDto;
 import leechungmu.goodsInform.entity.Dto.PromotionDto;
+import leechungmu.goodsInform.entity.Dto.UserDto;
 import leechungmu.goodsInform.entity.Item;
 import leechungmu.goodsInform.entity.Promotion;
+import leechungmu.goodsInform.entity.User;
 import leechungmu.goodsInform.repository.ItemRepository;
 import leechungmu.goodsInform.repository.PromotionRepository;
+import leechungmu.goodsInform.repository.UserRepository;
 import leechungmu.goodsInform.service.ItemService;
 import leechungmu.goodsInform.service.PromotionService;
+import leechungmu.goodsInform.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +22,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -34,6 +39,10 @@ public class ItemTest {
     PromotionService promotionService;
     @Autowired
     PromotionRepository promotionRepository;
+    @Autowired
+    UserService userService;
+    @Autowired
+    UserRepository userRepository;
     @Test
     void 상품등록(){
         Date end = new Date();
@@ -93,6 +102,103 @@ public class ItemTest {
 
         Optional<Promotion> findPromotion = promotionRepository.findById(promotionSaveId);
         assertEquals(findPromotion.isEmpty(), true);
+
+    }
+
+
+
+    @Test
+    void 사용자가구매할수있는상품정보(){
+        UserDto userDto1 = UserDto.builder()
+                .userId(1L)
+                .userName("이수경")
+                .userType("기업회원")
+                .userStat(true)
+                .build();
+        UserDto userDto2 = UserDto.builder()
+                .userId(2L)
+                .userName("최강석")
+                .userType("일반")
+                .userStat(true)
+                .build();
+
+        Long userId1 = userService.register(userDto1);
+        Long userId2 = userService.register(userDto2);
+        Optional<User> user1 = userRepository.findById(userId1);
+        Optional<User> user2 = userRepository.findById(userId2);
+        Date start = new Date();
+        Date end = new Date();
+        Calendar cal = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(start);
+        cal2.add(Calendar.MONTH, -1);
+        cal.setTime(end);
+        cal.add(Calendar.YEAR,1);
+        start = cal2.getTime();
+        end = cal.getTime();
+        ItemDto dto1 = ItemDto.builder()
+                .price(1000)
+                .itemId(1L)
+                .content("연필")
+                .enterpriseItem(false)
+                .startDate(start)
+                .endDate(end)
+                .stockQuantity(500)
+                .build();
+
+        ItemDto dto12 = ItemDto.builder()
+                .price(1000)
+                .itemId(12L)
+                .content("연필2")
+                .enterpriseItem(false)
+                .startDate(start)
+                .endDate(end)
+                .stockQuantity(0)
+                .build();
+        ItemDto dto2 = ItemDto.builder()
+                .price(2000)
+                .itemId(2L)
+                .content("지우개")
+                .enterpriseItem(true)
+                .startDate(start)
+                .endDate(end)
+                .stockQuantity(500)
+                .build();
+        ItemDto dto22 = ItemDto.builder()
+                .price(2000)
+                .itemId(22L)
+                .content("지우개2")
+                .enterpriseItem(true)
+                .startDate(start)
+                .endDate(end)
+                .stockQuantity(50)
+                .build();
+
+        cal.add(Calendar.YEAR,-2);
+        end = cal.getTime();
+
+        ItemDto dto3 = ItemDto.builder()
+                .price(4000)
+                .itemId(4L)
+                .content("볼펜")
+                .enterpriseItem(false)
+                .startDate(end)
+                .endDate(start)
+                .stockQuantity(500)
+                .build();
+
+
+        itemService.register(dto1);
+        itemService.register(dto12);
+        itemService.register(dto22);
+        itemService.register(dto2);
+        itemService.register(dto3);
+
+        List<ItemDto> enterpriseDtoList = itemService.read(user1.get().getUserType());
+        List<ItemDto> normalDtoList = itemService.read(user2.get().getUserType());
+
+        assertEquals(normalDtoList.size(), 1);
+        assertEquals(enterpriseDtoList.size(), 3);
 
     }
 }
--- goodsInform/src/test/java/leechungmu/goodsInform/PromotionTest.java
+++ goodsInform/src/test/java/leechungmu/goodsInform/PromotionTest.java
@@ -2,7 +2,10 @@ package leechungmu.goodsInform;
 
 import leechungmu.goodsInform.entity.Dto.ItemDto;
 import leechungmu.goodsInform.entity.Dto.PromotionDto;
+import leechungmu.goodsInform.entity.Dto.PromotionResponseDto;
+import leechungmu.goodsInform.entity.Item;
 import leechungmu.goodsInform.entity.Promotion;
+import leechungmu.goodsInform.repository.ItemRepository;
 import leechungmu.goodsInform.repository.PromotionRepository;
 import leechungmu.goodsInform.service.ItemService;
 import leechungmu.goodsInform.service.PromotionService;
@@ -26,6 +29,8 @@ public class PromotionTest {
     PromotionRepository promotionRepository;
     @Autowired
     ItemService itemService;
+    @Autowired
+    ItemRepository itemRepository;
     @Test
     void 프로모션등록(){
         Date end = new Date();
@@ -86,5 +91,59 @@ public class PromotionTest {
         Optional<Promotion> find = promotionRepository.findById(saveId);
         Promotion promotion = find.get();
         promotionRepository.delete(promotion);
+    }
+
+
+    @Test
+    void 상품에존재하는프로모션정보반환(){
+        Date end = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(end);
+        cal.add(Calendar.YEAR,1);
+        end = cal.getTime();
+        ItemDto itemDto = ItemDto.builder()
+                .price(5000)
+                .itemId(1L)
+                .content("연필")
+                .enterpriseItem(false)
+                .startDate(new Date())
+                .endDate(end)
+                .stockQuantity(500)
+                .build();
+        PromotionDto promotionDto1 = PromotionDto.builder()
+                .promotionId(1L)
+                .startDate(new Date())
+                .endDate(end)
+                .itemId(1L)
+                .ratio(true)
+                .sale(60)
+                .build();
+        PromotionDto promotionDto2 = PromotionDto.builder()
+                .promotionId(2L)
+                .startDate(new Date())
+                .endDate(end)
+                .itemId(1L)
+                .ratio(false)
+                .sale(3500)
+                .build();
+        cal.add(Calendar.YEAR,-2);
+        end = cal.getTime();
+        PromotionDto promotionDto3 = PromotionDto.builder()
+                .promotionId(3L)
+                .startDate(end)
+                .endDate(new Date())
+                .itemId(1L)
+                .ratio(false)
+                .sale(4900)
+                .build();
+        Long itemSaveId =itemService.register(itemDto);
+        Optional<Item> item = itemRepository.findById(itemSaveId);
+        Long saveId1= promotionService.register(promotionDto1);
+        Long saveId2= promotionService.register(promotionDto2);
+        Long saveId3= promotionService.register(promotionDto3);
+
+        PromotionResponseDto dto = promotionService.optimalPromotion(item.get());
+        assertEquals(dto.getPromotionId(),promotionDto2.getPromotionId());
+
     }
 }
